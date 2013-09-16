bd47061aa9003b261c56220711ddcb3ab17f5469
DanielFriedrich <daniel.friedrich@physik.uni-wuerzburg.de>
2013-09-16 21:12:23 +0200

    3 werte, plot 4 und mehr

---
+++ QHE/Data-Plots/11-4,2-zyklotron-B.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from pylab import *
+from scipy import *
+from scipy.optimize import *
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from numpy import *
+
+rc('font', family = 'serif', serif = 'STIXGeneral')
+fig = plt.figure(num=None, figsize=(7, 5), dpi=150, facecolor='w', edgecolor='k')
+
+#def f(U):
+#   return 297.0 -25.87*U + 1.908*U**2 - 0.4020*U**3;
+
+data = genfromtxt("4,2-Hallwiderstand-Magnetfeld_bereinigt.dat")
+data2 = genfromtxt("4,2-Laengsspannung.dat")
+#print data[100:,1]
+
+B = data[:8500,0]
+B2 = data2[:,1]
+#U_H = data[:,2]/10 * 1e-3
+#I_H = (data[:,3]/10 * 2e-3)/4981
+
+R_H = data[:8500,1]
+U_xx = data2[:,2]
+
+ax = fig.add_subplot(111)
+ax.plot(B, R_H, "b", label=u"Hallwiderstand")
+
+ax.set_xlabel(u"Magnetfeld $B$")
+ax.set_ylabel(u"Hallwiderstand $R_{\mathrm{Hall}}$")
+
+ax2 = ax.twinx()
+ax2.plot(B2,U_xx*1000, "r", label=u"Laengsspannung")
+#ax2.plt.ylim([0,10])
+
+ax2.set_ylabel(r"Laengsspannung $U_xx$")
+
+ax.legend(loc=3)
+ax2.legend(loc=4)
+
+# y = log(data[:,1]/(8e-3*1.63e-3*2.74e-3))
+# y = log(data[:,1]*(1.63e-3*2.74e-3)/(8e-3*4.2e-3))
+
+#X = x[0:14]
+#Y = y[0:14]
+
+#fitfunc = lambda p, x: p[0]*x + p[1]
+#errfunc = lambda p, x, y: fitfunc(p, x) - y
+#P, success = leastsq(errfunc, [1, 1], args=(X, Y))
+
+
+
+
+#plot(x, fitfunc(P, x), "r", label="Ausgleichsgerade")
+#plot(x, -0.0105*x + 0.9, "r--", label="Fehlergerade")
+#plot(x, -0.0077*x + 0.65, "r--")
+
+#plot([207, 207], [0.0, -4.5], "k", label="Inversionstemperatur")
+
+#fitfunc = lambda p, x: p[0] + p[1]*x + p[2]*x**2+ p[3]*x**3 + p[4]*x**4 + p[5]*x**5
+#errfunc = lambda p, x, y: fitfunc(p, x) - y
+#p, success = leastsq(errfunc, [1, 1, 1, 0, 1, 1, 1, 1, 1], args=(x, y))
+#R = linspace(min(x), max(x), 1000)
+#plot(R, fitfunc(p, R), label="Ausgleichende Kurve")
+
+#errorbar(x, y, yerr=len(x)*[0.04], xerr=len(x)*[1], fmt="g.", label="Messpunkte mit Fehlerbalken")
+
+
+
+show()
