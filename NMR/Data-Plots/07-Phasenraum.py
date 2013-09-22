ec3211af48743ef62131d49d00aa4353504877de
UMueller <Ulimllr@googlemail.com>
2013-09-22 15:44:58 +0200

    Drehwinkel

--- NMR/Data-Plots/07-Phasenraum.py
+++ NMR/Data-Plots/07-Phasenraum.py
@@ -1,16 +1,20 @@
 import numpy as np
-from scipy import optimize  
 import matplotlib.pyplot as plt
+from scipy import optimize  
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
 from pylab import *
 from matplotlib.ticker import AutoMinorLocator, AutoLocator
 rc('font', family = 'serif', serif = 'STIXGeneral')
 
 
+
 # ------------------  Eingaben  ------------
-Alle_Daten_plotten = False #True
-Einen_Datensatz_schoen_plotten = True
-read_start=10 # Erste Zeile, die eingelesen wird
-abgelesene_Werte_plotten = True #False 
+Alle_Daten_plotten = True #True
+Einen_Datensatz_schoen_plotten = False
+read_start=14 # Erste Zeile, die eingelesen wird
+abgelesene_Werte_plotten = False #False 
 # ------------------------------------------
 
 Plots=0
@@ -19,35 +23,63 @@ Plots=0
 # ------- Alle Daten Plotten ---------------
 if Alle_Daten_plotten==True:
     txt_filename_to_open=[]
-    for i in range(10):
-   05-phasenraum-010mV-minmax-FM.dat
-        filename="05-phasenraum-"+str(i)+"mV-minmax-FM.dat"
+    for i in range(18):
+        filename="05-phasenraum-0"+str(10+5*i)+"mV-minmax-FM.dat"
         txt_filename_to_open.append(filename)
-        
+    txt_filename_to_open.append("05-phasenraum-100mV-minmax-FM.dat")
+    # print txt_filename_to_open
+ 
+    
+    Frequenz=zeros(125000)
+    Amplitude=zeros(125000) 
+    Signal=zeros(125000)
+    z=0; nj=0
     for n in range(len(txt_filename_to_open)):
+        # print "reed file #", n
         data = [i.strip().split() for i in open(txt_filename_to_open[n]).readlines()]
 
-        t=[]
-        S=[]
-
         for i in range(len(data[read_start-1:])):
-            t=append(t,float(data[read_start-1+i][0]))
-            S=append(S,float(data[read_start-1+i][1]))
-            
-        Plots+=1    
-        plt.figure(Plots)
-        plt.xlabel(data[read_start-2][0:3])
-        plt.ylabel(data[read_start-2][3:6])
-        title(data[6])
-        font = {'family' : 'serif',
-                'weight' : 'normal',
-                'size'   : 20}
-        matplotlib.rc('font', **font)
-        subplots_adjust(left=0.18, bottom=0.12, right=0.96, top=0.92, wspace=0.2, hspace=0.2)
-        plt.plot(t,S,color="blue",label="Messwerte")
-        plt.legend(loc=0)	
-        # plt.savefig("Hallsteigung.pdf")
-        show()
+            if nj==200:
+                nj=0
+                Frequenz[z]=float(data[read_start-1+i][0])
+                Amplitude[z]=10+n*5
+                Signal[z]=float(data[read_start-1+i][1])
+                z+=1
+            nj+=1
+    for n in range(len(Amplitude)):
+        if Amplitude[n]==0:
+            Frequenz=Frequenz[0:n]
+            Amplitude=Amplitude[0:n]
+            Signal=Signal[0:n]
+            break
+       
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+print type(matplotlib.colors.Normalize(abs(Signal)))
+ax.scatter(Frequenz, Amplitude, Signal,s=50, c=abs(Signal), cmap="jet")#, rstride=1, cstride=1, cmap=cm.coolwarm,
+        #linewidth=0, antialiased=False)
+# #ax.set_zlim(-1.01, 1.01)
+
+# ax.zaxis.set_major_locator(LinearLocator(10))
+# ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+# fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show() 
+        # Plots+=1    
+        # plt.figure(Plots)
+        # plt.xlabel(data[read_start-2][0:3])
+        # plt.ylabel(data[read_start-2][3:6])
+        # title(data[6])
+        # font = {'family' : 'serif',
+                # 'weight' : 'normal',
+                # 'size'   : 20}
+        # matplotlib.rc('font', **font)
+        # subplots_adjust(left=0.18, bottom=0.12, right=0.96, top=0.92, wspace=0.2, hspace=0.2)
+        # plt.plot(t,S,color="blue",label="Messwerte")
+        # plt.legend(loc=0)	
+        # # plt.savefig("Hallsteigung.pdf")
+        # show()
 # ------------------------------------------ 
 
 
