34436c277246cd95b4656733541dc82edc084a67
UMueller <Ulimllr@googlemail.com>
2013-09-25 00:25:25 +0200

    Spindrehkurve und noch mehr

--- NMR/Data-Plots/11-Spindrehung.py
+++ NMR/Data-Plots/11-Spindrehung.py
@@ -8,13 +8,118 @@ from matplotlib.ticker import AutoMinorLocator, AutoLocator
 rc('font', family = 'serif', serif = 'STIXGeneral')
 import random
 # ------------------  Eingaben  ------------
-Alle_Daten_plotten = True #True
+Alle_Daten_plotten = False #True
 Einen_Datensatz_schoen_plotten = False
 read_start=14 # Erste Zeile, die eingelesen wird
 abgelesene_Werte_plotten = False #False 
 # ------------------------------------------
 
-Plots=0
+txt_filename_to_open=["05-phasenraum-AM-1500Hz-minmax.dat","05-phasenraum-AM-1500Hz-maxmin.dat"]
+
+
+data1 = [i.strip().split() for i in open(txt_filename_to_open[0]).readlines()]
+data2 = [i.strip().split() for i in open(txt_filename_to_open[1]).readlines()]
+theo_data=[i.strip().split() for i in open("ThData_von_Mathematica.dat").readlines()]
+# print theo_data
+
+t1=[]; t2=[]; S1=[]; S2=[]
+
+for i in range(len(data1[read_start-1:])):
+    t1=append(t1,float(data1[read_start-1+i][0])) 
+    S1=append(S1,float(data1[read_start-1+i][1]))
+    
+for i in range(len(data2[read_start-1:])):
+    t2=append(t2,float(data2[read_start-1+i][0]))
+    S2=append(S2,float(data2[read_start-1+i][1]))
+
+if  Alle_Daten_plotten == True :
+    plt.figure(0)
+    plt.xlabel(u"Intensit\xe4t [mV]")
+    plt.ylabel(u"Signal [arb.u.]")
+    title("Die zwei Resonanzkurven")
+    font = {'family' : 'serif',
+            'weight' : 'normal',
+            'size'   : 20}
+    #plt.xlim([0.049,0.0885])
+    plt.ylim([-1,0.4])
+    matplotlib.rc('font', **font)
+    subplots_adjust(left=0.18, bottom=0.12, right=0.96, top=0.92, wspace=0.2, hspace=0.2)
+    plt.plot(t1,S1,"b.", label="Messpunkte")
+    plt.plot(t2,S2,"r.", label="Messpunkte")
+    # plt.plot([0,1],[0,0],"k--")
+
+    plt.legend(loc=1)	
+  
+# Verschiebung anpassen  
+t=t1-0.0053548
+S=S1
+
+def biegen(kl,x):
+    return 2.0/kl[1]/kl[0]*x+x**2/kl[0]**2/kl[1]
+
+# fitfunc = lambda p, x: p[0] * cos(2*pi*p[1]*x+p[2])+p[3]
+# 
+# p0 = [5.0e-02, 2.6e3, -1.9, 1.8e-4]
+
+kl0=[1,1]
+def resiuum(kl):
+    y=copy(S)
+    
+    tide=0
+    newpoints=2.0/kl[1]/kl[0]*y+y**2/kl[0]**2/kl[1]
+    print "----------------------"
+    print "alte Punkte:",y
+    print "mit PArametern:", kl
+    print "neue Punkte:", newpoints
+    for n in range(len(t)):
+        if (t[n]>0.04000373 and t[n]<0.264918):
+            tide+=y[n]
+    print "Flaeche: ",tide
+    return abs(tide)        
+    
+#klbest= optimize.fmin(resiuum,kl0)
+# print "klbest: ", klbest
+# 0.264918
+# 0.04000373
+plotx=linspace(0,0.4,6400)
+ploty=exp(-4.5*plotx)
+
+plt.figure(0)
+plt.xlabel(u"Intensit\xe4t [mV]")
+plt.ylabel(u"Signal [arb.u.]")
+title("Spindrehungskurve")
+font = {'family' : 'serif',
+        'weight' : 'normal',
+        'size'   : 20}
+#plt.xlim([0.049,0.0885])
+plt.ylim([-0.85,0.85])
+plt.xlim([0,0.4])
+matplotlib.rc('font', **font)
+subplots_adjust(left=0.18, bottom=0.12, right=0.96, top=0.92, wspace=0.2, hspace=0.2)
+# plt.plot(t,S,"r.", label="Messpunkte")
+plt.plot(t,biegen([0.85,1.14],S),"r.", label="symmetrisierte Messpunkte") #{k -> 0.85, l -> 1.14};
+plt.plot(t,theo_data,"b.", label="theorie")
+# plt.plot(plotx,ploty,"r.", label="exp")
+# plt.plot(t,biegen([1,1],S),"g.", label="Messpunkte")
+# plt.plot(t1,S1,"b.", label="Messpunkte")
+# plt.plot(t2,S2,"r.", label="Messpunkte")
+# plt.plot([0,1],[0,0],"k--")
+
+plt.legend(loc=1)	 
+show()
+
+# f = file('kl-Daten.dat', 'w')
+# for n in range(len(S)):
+    # f.write(str(biegen([-0.64,-1.58],S)[n])+'\n')
+# f.close()
+
+
+
+
+
+
+
+
 
 
 # ------- Alle Daten Plotten ---------------
@@ -46,17 +151,40 @@ if Alle_Daten_plotten==True:
         Tide=0.0
         for n in range(len(x)):
             if (x[n]>0.02856 and x[n]<0.3929):
-                if biegen(p,y[n])>0: 
-                    Tide+=1
-                if biegen(p,y[n])<0: 
-                    Tide-=1
-        # print Tide
+                Tide+=biegen(p,y[n])**2
+        print p,Tide
         return Tide
     p=[random.random()*2,random.random()*2]
-    # while (abs(symmetrie(p,t,S))>130):
-        # p=[random.random()*2,random.random()*2]
-        # print p
-        # print symmetrie(p,t,S)
+    Tidebest=100000
+    while (True):
+        p=[(random.random()+1)*4,random.random()*1.1]
+        if abs(symmetrie(p,t,S))<Tidebest:
+            Tidebest=symmetrie(p,t,S)
+            print symmetrie(p,t,S)
+            Plots+=1    
+            plt.figure(Plots)
+            plt.xlabel("Frequenz [Hz]")
+            plt.ylabel("Signal [arb.u.]")
+            title("verschobene Resonanzkurven")
+            font = {'family' : 'serif',
+                    'weight' : 'normal',
+                    'size'   : 20}
+            #plt.xlim([0.049,0.0885])
+            plt.ylim([-1,0.4])
+            matplotlib.rc('font', **font)
+            subplots_adjust(left=0.18, bottom=0.12, right=0.96, top=0.92, wspace=0.2, hspace=0.2)
+            plt.plot(t,S,"b.", label="Messpunkte")
+            plt.plot(t,biegen([1.3,0.2],S),"r.", label="Messpunkte")
+
+            plt.plot([0,1],[0,0],"k--")
+
+            plt.legend(loc=1)	
+            show()
+                        
+            
+            
+        
+
     # # pbest, success = optimize.leastsq(residuals, p0, args=(t, S))
     # pbest, success = optimize.anneal(residuals, p0,args=(t,S))#,schedule='fast')#, T0=1e-0,Tf=1e-50,lower=[0,-10,1],upper=[0,10,1]
     Spins=((1-S/k)**2-1)/l
@@ -74,95 +202,13 @@ if Alle_Daten_plotten==True:
     matplotlib.rc('font', **font)
     subplots_adjust(left=0.18, bottom=0.12, right=0.96, top=0.92, wspace=0.2, hspace=0.2)
     plt.plot(t,S,"b.", label="Messpunkte")
-    # plt.plot(t,biegen(p,S),"b.", label="Messpunkte")
+    plt.plot(t,biegen([1.3,0.2],S), label="Messpunkte")
 
     plt.plot([0,1],[0,0],"k--")
-    # plt.plot(t2,S2,"g.", label="Messpunkte")
-    # plt.plot([1510,1528,1528,1528],[0.39,0.39,0.38,0.40],"r-",linewidth=2, label="Ablesepunkte")
-    # plt.plot([1510,1492,1492,1492],[0.39,0.39,0.38,0.40],"r-",linewidth=2)
-    # plt.plot([1510,1514,1506,1510],[0.39-0.005,0.37-0.005,0.37-0.005,0.39-0.005],"r-",linewidth=2)
-    # plt.plot(t1,linspace(out[0][0],out[0][0],len(t1)),"r",label="Messwerte", linewidth=3)
 
     plt.legend(loc=1)	
-    # plt.savefig("Hallsteigung.pdf")
     show()
 # ------------------------------------------ 
-
-
-# ------- Einen_Datensatz_schoen_plotten ---
-if Einen_Datensatz_schoen_plotten==True:
-    txt_filename_to_open=[]
-    for i in range(10):
-        filename="03-Resonanzfrequenz-"+str(i)+".dat"
-        txt_filename_to_open.append(filename)
-
-    data = [i.strip().split() for i in open(txt_filename_to_open[7]).readlines()]
-
-    t=[]
-    S=[]
-
-    for i in range(len(data[read_start-1:])):
-        t=append(t,float(data[read_start-1+i][0]))
-        S=append(S,float(data[read_start-1+i][1]))
-        
-    Plots+=1    
-    plt.figure(Plots)
-    plt.xlabel("Zeit [s]")
-    plt.ylabel("Messsignal [arb. u.]")
-    plt.xlim([0.049,0.0885])
-    plt.ylim([-1.02841,0.0441123])
-    title("Zeitdifferenz zwischen zwei Resonanzen \n  Analysatorfrquenz:4715023Hz")
-    font = {'family' : 'serif',
-            'weight' : 'normal',
-            'size'   : 20}
-    matplotlib.rc('font', **font)
-    subplots_adjust(left=0.13, bottom=0.11, right=0.97, top=0.86, wspace=0.2, hspace=0.2)
-    plt.plot(t,S,color="blue",label="Messwerte")
-    plt.plot([0.0537787,0.0609017],[-0.8,-0.8],color="red",label="abgelesene Zeitdifferenz")
-    plt.plot([0.0537787,0.0537787],[-0.8+0.005,-0.8-0.005],color="red")
-    plt.plot([0.0537787,0.0537787],[-0.8+0.005,-0.731546],"--",color="red")
-    plt.plot([0.0609017,0.0609017],[-0.8+0.005,-0.8-0.005],color="red")
-    plt.plot([0.0609017,0.0609017],[-0.8+0.005,-0.543839],"--",color="red")
-    plt.text(0.0574,-0.7732,u"$t_{12}$",va="center",ha="center",color="red")
-    plt.legend(loc=0)	
-# ------------------------------------------ 
-
-
-# ------- Abgelesene Werte plotten----------
-if abgelesene_Werte_plotten==True:
-    Frequenzen=array([4706.795,4707.792,4709.002,4710.266,4711.791,4712.76,4713.89,4715.023,4716.449,4717.478])
-    Delta_t=array([0.0175821,0.0150391,0.0135779,0.0122491,0.0105905,0.0095616,0.0083961,0.007123,0.0054044,0.0038974])
-    p0=[1,5,0.04]
-    fit = lambda p, x: p[0]+p[1]*cos(2*pi*x/p[2])
-
-    def residuals(p, x, y):
-        err = y-fit(p,x)
-        return err
-    out= optimize.leastsq(residuals, p0, args=(Delta_t, Frequenzen), full_output=1)
-    print out[0]
-    Plots+=1    
-    plt.figure(Plots)
-    plt.xlabel("Zeitdifferenz $t_{12}$ [s]")
-    plt.ylabel("Frequenzen in [kHz]")    
-    plt.xlim([0.002817,0.0203311])
-    plt.ylim([4704.51,4722.39])
-    title("Bestimmung der Resonanzfrequenz")
-    font = {'family' : 'serif',
-            'weight' : 'normal',
-            'size'   : 20}
-    matplotlib.rc('font', **font)
-    subplots_adjust(left=0.13, bottom=0.12, right=0.96, top=0.92, wspace=0.2, hspace=0.2)
-    plt.errorbar(Delta_t,Frequenzen,xerr=0.0002,yerr=0.002,label="bestimmte Zeitdifferenzen",fmt='r.')
-    xdata=linspace(0,0.02,1000)
-    plt.plot(xdata,fit(out[0],xdata),"b--",label="Cosinus-fit")
-    plt.plot([0.01,0.01],[4700,4712.31],"black",label="abgelesene Resonanzfrequenz")
-    plt.plot([0.00,0.01],[4712.31,4712.31],"black")
-    plt.legend(loc=0)	
-    # plt.savefig("Hallsteigung.pdf")
-# ------------------------------------------ 
-
-show()
-
 
 
 
